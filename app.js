783a859e89acab54e30a21d5e3c01a26a9f9996a
Ashutosh Yadav <ashutoshy7238@gmail.com>
2026-06-05 02:22:30 +0530

    Home route issue fixed

--- app.js
+++ app.js
@@ -96,8 +96,7 @@ app.use((req,res,next) => {
 // })
 
 app.get("/", (req,res) => {
-    console.log("Root route hit");
-    res.send("home route working");
+    res.redirect("/listing");
 });
 
 app.use("/listing", listingRouter);
